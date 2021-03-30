c4ec7f2297054115b67557ef9ae0778e9909a9ef
phamthaihoang1234 <70738519+phamthaihoang1234@users.noreply.github.com>
2021-03-30 13:38:00 +0700

    add_login_by_gmail

--- src/main/java/com/example/demo/controllers/SecurityController.java
+++ src/main/java/com/example/demo/controllers/SecurityController.java
@@ -1,19 +1,29 @@
 package com.example.demo.controllers;
 
+import com.example.demo.common.GooglePojo;
+import com.example.demo.common.GoogleUtils;
 import com.example.demo.model.Role;
 import com.example.demo.model.User;
 import com.example.demo.service.OrderSession.OrderService;
 import com.example.demo.service.product.ProductService;
 import com.example.demo.service.role.IRoleService;
 import com.example.demo.service.user.IUserService;
+import org.apache.http.client.ClientProtocolException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,6 +32,9 @@ import java.util.Set;
 public class SecurityController {
     @Autowired
     IUserService userService;
+
+    @Autowired
+    private GoogleUtils googleUtils;
 
     @Autowired
     IRoleService roleService;
@@ -39,6 +52,25 @@ public class SecurityController {
     public String homepage(Model model){
         model.addAttribute("productss", productService.findAll());
         return "index";
+    }
+
+    @RequestMapping("/login-google")
+    public String loginGoogle(HttpServletRequest request) throws ClientProtocolException, IOException {
+        String code = request.getParameter("code");
+
+        if (code == null || code.isEmpty()) {
+            return "redirect:/login?google=error";
+        }
+
+        String accessToken = googleUtils.getToken(code);
+
+        GooglePojo googlePojo = googleUtils.getUserInfo(accessToken);
+        UserDetails userDetail = googleUtils.buildUser(googlePojo);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetail, null,
+                userDetail.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "redirect:/";
     }
 
     @GetMapping("/shop")
